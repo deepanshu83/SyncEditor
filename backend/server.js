d6e4f95c5f83347cad7fc247c35a65a12dff85dd
deepanshu83 <dd893566039@gmail.com>
2026-04-25 16:14:36 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
